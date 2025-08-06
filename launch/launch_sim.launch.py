ce6b33b53334792835f4b0de8201e9f96e923796
rharper459 <rharper459@gmail.com>
2025-08-06 20:11:40 +0100

    lidar simulation now works and publishes data to /scan topic

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,24 +1,28 @@
 import os
- 
-from ament_index_python.packages import get_package_share_directory
- 
- 
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
- 
 from launch_ros.actions import Node
- 
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
  
  
 def generate_launch_description():
- 
+    
  
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
  
     package_name='Speed-e_bot' #<--- CHANGE ME
  
+    world = LaunchConfiguration('world')
+    world_path = '/home/lordnair/dev_ws/src/Speed-e_bot/worlds/obstacles.world'
+    declare_world = DeclareLaunchArgument(
+        name='world', default_value=world_path,
+        description='Full path to the world model file to load')
+    
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -26,34 +30,46 @@ def generate_launch_description():
     )
  
     # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
+    gazebo_server = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                launch_arguments={'gz_args': 'empty.sdf'}.items()
+                launch_arguments={'gz_args': ['-r -s -v1 ', world]}.items()
              )
+    
+    # Include the Gazebo client launch file, provided by the gazebo_ros package
+    gazebo_client = IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource([os.path.join(
+                        get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+                    )]), launch_arguments={'gz_args': '-g '}.items()
+    )
  
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_model = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_spawn_model.launch.py')]),
-                launch_arguments={'topic': 'robot_description',
-                                  'entity_name': 'my_bot'}.items())
+    spawn_bot = Node(
+                        package='ros_gz_sim', 
+                        executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'diff_bot',
+                                   '-z', '0.2'],
+                        output='screen'
+    )
     
-    # Bridge ROS topics and Gazebo messages for establishing communication
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            'config_file': os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml'),
-            'qos_overrides./tf_static.publisher.durability': 'transient_local',
-        }],
-        output='screen'
+    # Launch the Gazebo-ROS bridge
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',]
     )
     
     # Launch them all!
     return LaunchDescription([
+        declare_world,
         rsp,
-        gazebo,
-        spawn_model,
-        bridge,
+        gazebo_server,
+        gazebo_client,
+        ros_gz_bridge,
+        spawn_bot
     ])
